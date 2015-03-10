eab3a5a1551077c769853e462ef70e3e16eb37e9
EclecticKlos <dsklos@gmail.com>
2015-03-10 11:22:05 -0700

    Game announces winner, still does not break out of game nor offer to reset the board.

--- action.js
+++ action.js
@@ -35,48 +35,74 @@
 
 
 
-// $(document).ready(function() {
-//   if ( event.which != 32 ) {
-//     document.write("GO!")
 
-//   } else {
-//     document.write("Press 'space bar' to begin the race")
-//   }
-// });
 
-// var gameplayMessage = "Press space bar";
-// function displayMessage() {
-//   document.write(gameplayMessage);
-// }
-// displayMessage();
+
+
+
+$(document).ready(function createMessage() {
+var messageForPlayers = document.getElementById("message");
+  messageForPlayers.innerHTML = "Press the space bar to begin the race.";
+});
 
 $(document).ready(function () {
   $(document).keyup(function go( event ){
     if ( event.which == 32) {
 
-      // gameplayMessage = "GO!";
-      // displayMessage(gameplayMessage);
-      var i = 1;
-      $(document).ready(function player1Strip(){
-        $(document).keyup(function( event ){
-          if ( event.which == 65) {
-            $("#player1_strip").children("td:nth-of-type(" + i + ")").css( "background-color", "red");
-            $("#player1_strip").children("td:nth-of-type(" + (i - 1) + ")").css( "background-color", "transparent");
-            i++;
-          }
-        });
+      $(document).ready(function createMessage() {
+        var messageForPlayers = document.getElementById("message");
+        messageForPlayers.innerHTML = "READY SET GO!";
       });
 
+      var i = 1;
       var t = 1;
-      $(document).ready(function player2Strip(){
-        $(document).keyup(function( event ){
-          if ( event.which == 76) {
-            $("#player2_strip").children("td:nth-of-type(" + t + ")").css( "background-color", "blue");
-            $("#player2_strip").children("td:nth-of-type(" + (t - 1) + ")").css( "background-color", "transparent");
-            t++;
-          }
+        $(document).ready(function player1Strip(){
+          $(document).keyup(function( event ){
+            if ( event.which == 65) {
+              $("#player1_strip").children("td:nth-of-type(" + i + ")").css( "background-color", "red");
+              $("#player1_strip").children("td:nth-of-type(" + (i - 1) + ")").css( "background-color", "transparent");
+              i++;
+              if (i === 10) {
+                $(document).ready(function createMessage() {
+                  var messageForPlayers = document.getElementById("message");
+                  messageForPlayers.innerHTML = "Player 1 wins!";
+                });
+              }
+            }
+          });
         });
-      });
+
+        $(document).ready(function player2Strip(){
+          $(document).keyup(function( event ){
+            if ( event.which == 76) {
+              $("#player2_strip").children("td:nth-of-type(" + t + ")").css( "background-color", "blue");
+              $("#player2_strip").children("td:nth-of-type(" + (t - 1) + ")").css( "background-color", "transparent");
+              t++;
+              if (t === 10) {
+                $(document).ready(function createMessage() {
+                  var messageForPlayers = document.getElementById("message");
+                  messageForPlayers.innerHTML = "Player 2 wins!";
+                });
+              }
+            }
+          });
+        });
+      // if (i === 10) {
+      //     $(document).ready(function createMessage() {
+      //       var messageForPlayers = document.getElementById("message");
+      //       messageForPlayers.innerHTML = "Player 1 wins!";
+      //     });
+      // } else if (t === 10) {
+      //     $(document).ready(function createMessage() {
+      //       var messageForPlayers = document.getElementById("message");
+      //       messageForPlayers.innerHTML = "Player 1 wins!";
+      //     });
+      // } else {
+      //     $(document).ready(function createMessage() {
+      //       var messageForPlayers = document.getElementById("message");
+      //       messageForPlayers.innerHTML = "I don't know what the fudge happened!";
+      //     });
+      // }
 
     }
   });
